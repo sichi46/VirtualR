cf8d43893bf8f0c54df46317020fbb19f7234cfd
Sichi46 <willsonmulenga7@gmail.com>
2024-08-19 13:23:12 +0200

    Changes

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import FeatureSection from "./components/FeatureSection";
+import Workflow from "./components/Workflow";
+import Pricing from "./components/Pricing";
+import Testimonial from "./components/Testimonial";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeatureSection />
+        <Workflow />
+        <Pricing />
+        <Testimonial />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
